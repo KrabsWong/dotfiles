af3007a17ee07bbd2178567009e2e925ca4f733e
Yooooo <pangxie.wdy@gmail.com>
2025-04-13 03:21:44 +0800

    feat: lsp server

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -194,7 +194,10 @@ return {
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {},
+      eslint = {},
+      vuels = {},
+      marksman = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
