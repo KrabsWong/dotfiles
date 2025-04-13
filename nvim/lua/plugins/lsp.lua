4dd1a80432ccb0798ed8c173749bfe3e2ef6c761
Yooooo <pangxie.wdy@gmail.com>
2025-04-13 18:57:22 +0800

    feat: update keymap for blink

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -180,7 +180,8 @@ return {
     --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
     -- local capabilities = require('blink.cmp').get_lsp_capabilities()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local orginal_capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = require("blink-cmp").get_lsp_capabilities(orginal_capabilities)
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
     --
